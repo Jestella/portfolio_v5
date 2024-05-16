85d2e541e33bbe75d5edaa36edf44869617aa33f
jestella <jestellaa@gmail.com>
2024-05-15 23:06:27 -0400

    add sass and  update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,92 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className="container">
+        {/* <MovingIcons /> */}
+        <section id="intro">
+          <div className="home-intro">
+            <h1>Stella Lee.</h1>
+            <h1>
+              A front-end Developer
+              <br />
+              based in Toronto,
+              <br />
+              who loves everything
+              <br /> that has to do with
+              <br />
+              art, design & technology.
+            </h1>
+            <div className="message">
+              <p>
+                "Hi 👋🏻,
+                <br />
+                this website is currently under construction
+                <br />
+                and will be updated soon!"
+                <br />
+              </p>
+            </div>
+          </div>
+        </section>
+        <section id="skills">
+          <div className="skills-container">
+            <ul className="skills-list">
+              <li># Web</li>
+              <li># App</li>
+              <li># IoT</li>
+              <li># UI/UX</li>
+            </ul>
+            <ul className="skills-list">
+              <li># Angular</li>
+              <li># React</li>
+              <li># Next.js</li>
+              <li># TypeScript</li>
+              <li># Bootstrap</li>
+            </ul>
+            <ul className="skills-list">
+              <li># Shopify</li>
+              <li># WordPress</li>
+              <li># Figma</li>
+            </ul>
+          </div>
+        </section>
+        <section id="featured-work">
+          <div className="featured-projects">
+            <p className="section-title">/* Work */</p>
+          </div>
+          <div className="featured-button-container">
+            <button className="button-more">
+              <Link href={"work"}>View More</Link>
+            </button>
+          </div>
+        </section>
+        <section>
+          <div className="home-bottom">
+            <div className="home-devlog">
+              <p>
+                Development Journey 👩🏻‍💻 &rarr;{" "}
+                <button>
+                  <Link href={"devlog"}>devlog</Link>
+                </button>
+              </p>
+            </div>
+            <div className="home-lab">
+              <p>
+                Random Experiments 💡 &rarr;{" "}
+                <button>
+                  <Link href={"lab"}>lab</Link>
+                </button>
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      );
     </main>
   );
 }
